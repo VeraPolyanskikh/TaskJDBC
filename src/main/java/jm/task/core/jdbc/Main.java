527f598a03930baee5d7d437c38c7107bc64eec5
VeraPolyanskikh <vpcat3@gmail.com>
2021-01-21 01:45:38 +0300

    still JDBC, but some fixes and decorations

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,10 +2,6 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
-
-import java.sql.SQLException;
-
-
 public class Main {
     public static void main(String[] args) {
 
